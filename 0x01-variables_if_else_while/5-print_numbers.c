a7594bef576fca1ef83f94dd56e10e1226279e0e
ahmed-seryo <ahmad_sayed2022@yahoo.com>
2023-06-16 18:12:22 -0700

    alx

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+
 /**
  * main - entry point
- * Descrption: make numbers from 0-9
- * Return: 0
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: always 0 (success)
  */
+
 int main(void)
 {
 	int digit = 0;
 
-	while (digit < 10)
+	while (digit < '10')
 	{
-		char buffer[11];
-
-		snprintf(buffer, 11, "%d", digit);
-		printf("%s", buffer);
+		printf("%i", digit);
 		digit++;
 	}
+	printf("\n");
 	return (0);
 }
+
